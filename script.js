8a8dca86107f00fa5c5bdd7116994e77b5b514ec
Kylie Smaldon <k_smaldon@outlook.com>
2022-01-04 17:28:59 +0000

    Add makeGrid function

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const gridContainer_div = document.querySelector(".grid-container")
 
 
 //MAKE GRID
-function makeGrid(num){
-for(let i=0; i<num; i++){
-   let square = document.createElement('div')
-   gridContainer_div.appendChild(square); 
-   square.classList.add("square")
-    }
-    
-}
+function makeGrid(col, row){
+    for(let i=0; i<col*row; i++){
+        let square = document.createElement('div');
+        square.classList.add("square");
+        gridContainer_div.appendChild(square);
 
-makeGrid(16);
+   
+        }
+    gridContainer_div.style.gridTemplateColumns = `repeat(${col},auto)`; 
+    gridContainer_div.style. gridTemplateRows = `repeat(${row},auto)`;
+    }
+
+
+
+console.log(gridContainer_div.style.gridTemplateColumn)
+
+makeGrid(5,5);
