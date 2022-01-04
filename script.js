02bca1f4a764170a95d8743b918cd16871be1a57
Kylie Smaldon <k_smaldon@outlook.com>
2022-01-04 22:42:13 +0000

    Add hover function

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 //DOM ELEMENTS
 const reset_button = document.getElementById("reset");
 const gridContainer_div = document.querySelector(".grid-container")
+let square
 
+console.log(reset_button);
 
 //MAKE GRID
 function makeGrid(col, row){
+
     for(let i=0; i<col*row; i++){
-        let square = document.createElement('div');
+        square = document.createElement('div');
         square.classList.add("square");
         gridContainer_div.appendChild(square);
-
-   
-        }
+    }
+    //GRID STYLE
     gridContainer_div.style.gridTemplateColumns = `repeat(${col},auto)`; 
     gridContainer_div.style. gridTemplateRows = `repeat(${row},auto)`;
-    }
+    
+    //HOVER EFFECT
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square)=>{
+        square.addEventListener('mouseover', ()=>{
+            square.classList.add("color-change");   
+        })
+
+        reset_button.addEventListener('click',()=>{
+        square.classList.remove("color-change");
+    })
+    })
+    
+    
+    
+
+}
 
 
 
-console.log(gridContainer_div.style.gridTemplateColumn)
 
-makeGrid(5,5);
+makeGrid(100,100);
