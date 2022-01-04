592e1c0a762ee3b35363bb1e63633a7cdd4cb856
Kylie Smaldon <k_smaldon@outlook.com>
2022-01-04 11:02:32 +0000

    Add makeGrid function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello")
+//DOM ELEMENTS
+const reset_button = document.getElementById("reset");
+const gridContainer_div = document.querySelector(".grid-container")
+
+
+//MAKE GRID
+function makeGrid(num){
+for(let i=0; i<num; i++){
+   let square = document.createElement('div')
+   gridContainer_div.appendChild(square); 
+   square.classList.add("square")
+    }
+    
+}
+
+makeGrid(16);
